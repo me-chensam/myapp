3642e5b70a741586c918add1d79163e8361e7a6d
chen <sm.chen007@gmail.com>
2023-09-29 10:44:39 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 function App() {
-  const [data, setData] = React.useState("Not Found");
 
   return (
-    <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-    </>
+    <h1>Hello World</h1>
   );
 }
 
